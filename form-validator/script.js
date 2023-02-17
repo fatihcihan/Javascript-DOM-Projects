eefeb0e6da861d6061a1ad4d915a4234ae48986d
Fatih <fatihcihan666@gmail.com>
2023-02-17 03:00:01 +0300

    application of other validation rules

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
+const phone = document.getElementById('phone');
 const password = document.getElementById('password');
 const repassword = document.getElementById('repassword');
 
@@ -18,10 +19,10 @@ function success(input) {
 
 function checkEmail(input) {
     const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if (re.test(input.value)){
+    if (re.test(input.value)) {
         success(input);
-    }else{
-        error(input,'an incorrect e-mail address')
+    } else {
+        error(input, 'an incorrect e-mail address')
     }
 
 }
@@ -36,8 +37,36 @@ function checkRequired(inputs) {
     });
 }
 
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        error(input, `${input.id} must be at least ${min} chracters`);
+    } else if (input.value.length > max) {
+        error(input, `${input.id} must be a maximum of ${max} chracters`);
+    } else {
+        success(input);
+    }
+}
+
+function checkPasswords(inputPassword, inputRepassword) {
+    if (inputPassword.value !== inputRepassword.value) {
+        error(inputPassword, 'passwords do not match')
+    }
+}
+
+function checkPhone(input) {
+    var exp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (!exp.test(input.value)) {
+        error(input, 'phone must have 10 characters');
+    }
+}
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    checkRequired([username, email, password, repassword]);
+    checkRequired([username, email, password, repassword, phone]);
     checkEmail(email);
+    checkLength(username, 7, 15);
+    checkLength(password, 7, 12);
+    checkLength(repassword, 7, 12);
+    checkPasswords(password, repassword);
+    checkPhone(phone);
 })
